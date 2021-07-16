f811c5e5d9be044cbe3e8e6b417e4907cd4a83f8
matiseisdedos <matiasseisdedosg@gmail.com>
2021-07-16 10:40:11 -0300

    En proceso

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-conn.sync({ force: true }).then(() => {
+conn.sync().then(() => {
   server.listen(3001, () => {
     console.log('Now listening at 3001');
   });
--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,14 +1,36 @@
 const { Router } = require("express")
 const { Country } = require("../db")
+const { Op } = require('sequelize')
 const axios = require("axios")
 
 const router = Router();
+/* GET /countries?name="...":
+Obtener los países que coincidan con el nombre pasado como query parameter (No necesariamente tiene que ser una matcheo exacto)
+Si no existe ningún país mostrar un mensaje adecuado*/
+
 
 router.get("/countries", async (req, res) => {
     let database = await Country.findAll()
     let api = await axios.get("https://restcountries.eu/rest/v2/all")
     let countries = [...database, ...api.data]
     let firstTen = []
+    const name = req.query.name
+    console.log(name);
+    let search = []
+    // if (name !== undefined) {
+    //     if (name.length > 0 && name.length < 3) {
+    //         search = countries.filter(e => e.alpha2Code === name)
+    //         if (search.length === 0) return res.send('No se encontro el pais ingresado')
+    //         return res.send(search)
+    //     } else if (name.length === 3) {
+    //         search = countries.filter(e => e.alpha3Code === name)
+    //         if (search.length === 0) return res.send('No se encontro el pais ingresado')
+    //         return res.send(search)
+    //     }
+    //     else {
+    //         return res.send('No se encontro el pais ingresado')
+    //     }
+    // }
     for (let i = 0; i < 10; i++) {
         firstTen.push(countries[i])
     }
